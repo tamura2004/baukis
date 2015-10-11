14ec13dd59de8f7d62507e6b72d412b1aab39a1e
tamura <tamura2004@gmail.com>
2015-10-11 02:41:49 +0000

    p201

--- app/controllers/admin/top_controller.rb
+++ app/controllers/admin/top_controller.rb
@@ -1,6 +1,9 @@
 class Admin::TopController < Admin::Base
   def index
-    # raise IpAddressRejected
-    render action: "index"
+    if current_administrator
+      render action: "dashboard"
+    else
+      render action: "index"
+    end
   end
 end
