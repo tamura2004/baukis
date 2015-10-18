79727fbfa50172617b6a65cda463816d0a95d4b6
tamura <tamura2004@gmail.com>
2015-10-18 02:28:43 +0000

    p377

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -1,1 +1,1 @@
-Rails.application.config.assets.precompile += %w( staff.css admin.css customer.css )
+Rails.application.config.assets.precompile += %w( staff.css admin.css customer.css admin.js staff.js customer.js )
